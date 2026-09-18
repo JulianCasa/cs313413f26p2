381a2871ebbaefdc4e56e78199a5505c79ab2dea
Julian Casalez <juliangc1211@gmail.com>
2026-09-18 00:09:42 -0500

    TestIterator.java solutions added. Updates made to the README.txt file

--- src/test/java/cs271/lab/list/README.txt
+++ src/test/java/cs271/lab/list/README.txt
@@ -1,3 +1,4 @@
+Project 2 Report
 //TestList.java TODO questions
 
  list.remove(5); // TODO Question: What does this method do?
@@ -5,3 +6,14 @@
 
 list.remove(Integer.valueOf(5)); // TODO Question: What does this one do?
         This method searches the list for the Integer value 5 and removes it regardless of its position
+
+//TestIterator TODO Questions
+// TODO Question: Also try with a LinkedList - does it make any difference?//
+        There is very little difference when running the program using arraylist versus linked list.
+        There was also no issue in the methods, most likely because both classes implement the list
+        interface
+
+i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
+        If the method is called within the provided while loop, the program breaks because all
+        the instances of the integer 77 are removed in the first loop. If implemented ouyside a loop
+        it removes all instances of 77 at once
--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -5,10 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +20,7 @@ public class TestIterator {
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
     // TODO Question: Also try with a LinkedList - does it make any difference?
+    //list = new LinkedList<>();
   }
 
   @AfterEach
@@ -49,17 +48,17 @@ public class TestIterator {
     assertEquals(33, i.next().intValue());
     // TODO fix the expected values in the assertions below
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(44, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(55, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(66, i.next().intValue());
     assertFalse(i.hasNext());
   }
 
@@ -81,7 +80,8 @@ public class TestIterator {
     // TODO using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
+    assertEquals(List.of(33,44,55,66), list);
+
   }
 
   @Test
@@ -98,6 +98,12 @@ public class TestIterator {
     // TODO use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    final var i = list.iterator();
+
+    while (i.hasNext()){
+      sum += i.next();
+      n++;
+    }
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
   }
